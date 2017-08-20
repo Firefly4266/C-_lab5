bd0ef31adc81a8534edb9328e45ad545ca91d0f3
Firefly4266 <gaines.johnp@gmail.com>
2017-08-20 13:39:38 -0700

    Part 1 completed.

---
+++ ContactsRedux/TestContactsRedux.cs
@@ -0,0 +1,49 @@
+﻿using System; 
+
+public class TestContactsRedux
+{
+    public static void Main()
+    {
+        InputWrapper iw = new InputWrapper();
+        Console.WriteLine("Enter command,  <quit> to exit.");
+        string cmd = iw.getString("> ");
+        while (cmd != "quit")
+        {
+            Console.WriteLine("The {0} command was entered.", cmd);
+            switch (cmd)
+            {
+                case "add":
+                    {
+                        Console.WriteLine("add command");
+                            break;
+                    }
+                case "forward":
+                    {
+                        Console.WriteLine("forward command");
+                        break;
+                    }
+                case "reverse":
+                    {
+                        Console.WriteLine("reverse command");
+                        break;
+                    }
+                case "find":
+                    {
+                        Console.WriteLine("find command");
+                        break;
+                    }
+                case "remove":
+                    {
+                        Console.WriteLine("remove command");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("add, forward, reverse, find, remove are the only commands available");
+                        break;
+                    }
+            }
+            cmd = iw.getString("> ");
+        }
+    }
+}
